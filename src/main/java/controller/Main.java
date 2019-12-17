c12d41f08f1c27934d30335d86add60157ae4789
Nelson Estevão <nelsonmestevao@gmail.com>
2019-12-17 10:49:04 +0000

    :sparkles: Add album cover

--- src/main/java/controller/Main.java
+++ src/main/java/controller/Main.java
@@ -1,15 +1,23 @@
 package controller;
 
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import java.util.Set;
 
+import com.mpatric.mp3agic.ID3v2;
+import com.mpatric.mp3agic.InvalidDataException;
+import com.mpatric.mp3agic.Mp3File;
+import com.mpatric.mp3agic.UnsupportedTagException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.control.TableColumn.CellEditEvent;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
@@ -19,9 +27,13 @@ import exceptions.LackOfPermissions;
 import model.MediaCenter;
 import model.MediaTableRow;
 
+import javax.imageio.ImageIO;
+
 public final class Main {
     private static Helper helper;
     private static MediaCenter model;
+
+    private static final String USER_DATA_DIR = System.getenv("SYM_USER_DATA_DIR");
 
     public static void init(final Helper hlpr, final MediaCenter mdl) {
         Main.helper = hlpr;
@@ -62,15 +74,19 @@ public final class Main {
     private TextField searchBar;
 
     @FXML
-    private MediaView videoPlayer;
-
-    @FXML
     private Button playButton;
 
     @FXML
     private TableView<MediaTableRow> musicTable;
 
+    @FXML
     private MediaPlayer mediaPlayer;
+
+    @FXML
+    private MediaView videoPlayer;
+
+    @FXML
+    private ImageView imageCoverAlbum;
 
     private String playingSong;
 
@@ -199,6 +215,7 @@ public final class Main {
                     this.mediaPlayer = new MediaPlayer(new Media(new File(fileLocation).toURI().toString()));
                     this.mediaPlayer.setAutoPlay(true);
                     this.videoPlayer.setMediaPlayer(mediaPlayer);
+                    this.setAlbumCover(fileLocation);
                     this.playingSong = name;
                     this.playButton.setText("II");
                 } catch (IOException e) {
@@ -214,6 +231,7 @@ public final class Main {
                 this.mediaPlayer = new MediaPlayer(new Media(new File(fileLocation).toURI().toString()));
                 this.mediaPlayer.setAutoPlay(true);
                 this.videoPlayer.setMediaPlayer(mediaPlayer);
+                this.setAlbumCover(fileLocation);
                 this.playingSong = name;
                 this.playButton.setText("II");
             } catch (IOException e) {
@@ -226,6 +244,26 @@ public final class Main {
             this.playingSong = "";
             this.playButton.setText("▸");
         });
+    }
+
+    void setAlbumCover(final String filename) {
+        try {
+            Mp3File song = new Mp3File(filename);
+            if (song.hasId3v2Tag()) {
+                ID3v2 id3v2tag = song.getId3v2Tag();
+                BufferedImage img = ImageIO.read(new ByteArrayInputStream(id3v2tag.getAlbumImage()));
+                File outputfile = new File(USER_DATA_DIR + "cover.jpg");
+                ImageIO.write(img, "jpg", outputfile);
+                this.imageCoverAlbum.setImage(new Image(outputfile.toURI().toString()));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InvalidDataException e) {
+            this.imageCoverAlbum.setImage(null);
+            e.printStackTrace();
+        } catch (UnsupportedTagException e) {
+            e.printStackTrace();
+        }
     }
 
     @FXML
@@ -246,6 +284,7 @@ public final class Main {
         if (this.mediaPlayer != null)
             this.mediaPlayer.stop();
         this.videoPlayer.setMediaPlayer(null);
+        this.imageCoverAlbum.setImage(null);
         musicTable.getItems().clear();
     }
 }
